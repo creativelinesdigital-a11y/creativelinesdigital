1c5c6c6231b6c88630c1ab09649e2d7531488636
syed saqib <s.s.barkaat@gmail.com>
2025-09-27 17:54:31 +0000

    put this image as the logo in the header:
    https://i.ibb.co/HTChB0DY/cr

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,24 +1,15 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const Logo = ({ className }: { className?: string }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 160 30"
-    width="160"
-    height="30"
-    className={cn("fill-current text-foreground", className)}
-  >
-    <text
-      x="0"
-      y="20"
-      fontFamily="Inter, sans-serif"
-      fontSize="14"
-      fontWeight="bold"
-      textAnchor="start"
-    >
-      CREATIVE LINES DIGITAL
-    </text>
-  </svg>
+  <Image
+    src="https://i.ibb.co/HTChB0DY/creative-lines-digital-logo-transparent.png"
+    alt="Creative Lines Digital Logo"
+    width={160}
+    height={30}
+    className={cn("invert dark:invert-0", className)}
+    priority
+  />
 );
 
 export default Logo;
