94567c24843685f5789471ea6d36117b146e8274
syed saqib <s.s.barkaat@gmail.com>
2025-09-27 17:49:35 +0000

    please replease and add this logo in the website

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,35 +1,13 @@
-import * as React from "react";
+import Image from "next/image";
 
-const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width="220"
-    height="32"
-    viewBox="0 0 220 36"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      d="M24.736 35.216L12.592 18.064L24.736 0.896H34.12L19.48 18.064L34.12 35.216H24.736Z"
-      className="fill-primary"
-      transform="scale(0.8) translate(-10, 4)"
-    />
-    <path
-      d="M15.4 35.216L3.256 18.064L15.4 0.896H24.784L10.144 18.064L24.784 35.216H15.4Z"
-      className="fill-primary/70"
-      transform="scale(0.8) translate(-10, 4)"
-    />
-    <text
-      x="28"
-      y="27"
-      fontFamily="Inter, sans-serif"
-      fontSize="16"
-      fontWeight="bold"
-      className="fill-foreground uppercase"
-    >
-      Creative Lines Digital
-    </text>
-  </svg>
+const Logo = () => (
+  <Image
+    src="/logo.png"
+    alt="Creative Lines Digital Logo"
+    width={180}
+    height={30}
+    priority
+  />
 );
 
 export default Logo;
