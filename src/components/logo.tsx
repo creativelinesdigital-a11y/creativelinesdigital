d106fa7a2cb3eadf1682865684fd4fbe030cc67e
syed saqib <s.s.barkaat@gmail.com>
2025-09-27 18:25:11 +0000

    please make the logo a little bit wide and increase hight a little bit b

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ const Logo = ({ className }: { className?: string }) => (
   <Image
     src="https://i.ibb.co/Swp9938S/final-logo-trans.png"
     alt="Creative Lines Digital Logo"
-    width={140}
-    height={26}
+    width={150}
+    height={28}
     className={cn("invert dark:invert-0", className)}
     priority
   />
