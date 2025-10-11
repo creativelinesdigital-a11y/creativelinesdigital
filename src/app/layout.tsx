d9ea20dcdb67112519363cae09666041df2705e1
syed saqib <s.s.barkaat@gmail.com>
2025-10-11 15:29:23 +0000

    https://i.ibb.co/spN9dH0Y/creative-lines-digital-logo-transparent.png
    P

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="https://i.ibb.co/spN9dH0Y/creative-lines-digital-logo-transparent.png" type="image/png" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
